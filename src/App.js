d99e89653a2bcbeca08900672e7ecd12e94a3326
kwas0 <kwas05@hotmail.com>
2023-09-30 20:42:42 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TwoButtonsPage from "./pages/TwoButtonsPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <span>TEST</span>
+          {/*<span>TEST</span>*/}
             <Route path='/two-buttons' element={<TwoButtonsPage />} />
         </Routes>
       </div>
