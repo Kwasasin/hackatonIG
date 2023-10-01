f6be672162db4fbb3cd4406adcc46114741fa5e9
kwas0 <kwas05@hotmail.com>
2023-10-01 10:32:03 +0200

    search bar added 2

--- src/pages/SearchBarComponent.js
+++ src/pages/SearchBarComponent.js
@@ -7,10 +7,12 @@ const SearchBarComponent = () => {
         <Container className="searchContainer1">
             <div className="input-group">
                 <div className="form-outline">
+                    <label className="form-label" htmlFor="form1">Search patient database</label>
                     <input id="search-input" type="search" id="form1" className="form-control"/>
-                    <label className="form-label" htmlFor="form1">Search</label>
+                    <h1> </h1>
                 </div>
-                <button id="search-button" type="button" className="btn btn-primary">Test
+                <button id="search-button" type="button" className="btn btn-primary">
+                    Search patient
                     <i className="fas fa-search"></i>
                 </button>
             </div>
