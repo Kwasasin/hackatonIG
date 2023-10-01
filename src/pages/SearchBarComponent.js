b0991ee15333c61d3d44b5e20992e5522cc63d23
kwas0 <kwas05@hotmail.com>
2023-10-01 10:28:12 +0200

    search bar added

--- src/pages/SearchBarComponent.js
+++ src/pages/SearchBarComponent.js
@@ -4,18 +4,16 @@ import {Col, Container} from "react-bootstrap";
 
 const SearchBarComponent = () => {
     return (
-        <Container>
-            <Col className="justify-content-center">
-            <div className="input-group justify-content-center">
+        <Container className="searchContainer1">
+            <div className="input-group">
                 <div className="form-outline">
                     <input id="search-input" type="search" id="form1" className="form-control"/>
                     <label className="form-label" htmlFor="form1">Search</label>
                 </div>
-                <button id="search-button" type="button" className="btn btn-primary">
+                <button id="search-button" type="button" className="btn btn-primary">Test
                     <i className="fas fa-search"></i>
                 </button>
             </div>
-            </Col>
         </Container>
     );
 };
